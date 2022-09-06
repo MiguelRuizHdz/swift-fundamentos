4cf73656806651332dbd5f0898323558bf8ff0a6
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-06 10:32:38 -0500

    Ejercicio con una app: Objetivo de pasos

--- 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/2. Ejercicio con una app - Objetivo de pasos.xcplaygroundpage/Contents.swift
+++ 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/2. Ejercicio con una app - Objetivo de pasos.xcplaygroundpage/Contents.swift
@@ -5,11 +5,11 @@
 
  Tu app de seguimiento de actividad física necesita conocer la cantidad de pasos que el usuario quiere dar por día. Crea la constante `goalSteps` (objetivo de pasos) y configúrala en 10,000.
  */
- 
+let goalSteps = 10_000
 
 //:  Usa dos funciones `print` (imprimir) para imprimir dos líneas separadas en la consola. La primera línea debe decir "Tu objetivo de pasos para el día de hoy es el siguiente:" y la segunda debe imprimir el valor de `goalSteps` (objetivo de pasos) haciendo referencia a tu constante.
- 
-
+print("Tu objetivo de pasos para el día de hoy es el siguiente:")
+print(goalSteps)
 /*:
 [Anterior](@previous) | Página 2 de 10 | [Siguiente: Ejercicio: Variables](@next)
  */
