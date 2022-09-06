c905c564389aad296e07bf6d8a8af245882fe942
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-06 10:54:59 -0500

    Ejercicio: Tipos y seguridad de tipos

--- 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/7. Ejercicio - Tipos y seguridad de tipos.xcplaygroundpage/Contents.swift
+++ 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/7. Ejercicio - Tipos y seguridad de tipos.xcplaygroundpage/Contents.swift
@@ -3,11 +3,13 @@
  
  Declara dos variables, una denominada `firstDecimal` (primer decimal) y la otra, `secondDecimal` (segundo decimal). Ambas deben contener valores decimales. Mira ambos tipos presionando la tecla Opción y haciendo clic en el nombre de la variable.
  */
- 
+var firstDecimal = 2.3
+var secondDecimal = 35.33
 
 //:  Declara una variable denominada `trueOrFalse` (verdadero o falso) y asígnale un valor booleano. Intenta asignarla al `firstDecimal` (primer decimal) de esta forma: `firstDecimal = trueOrFalse` (primer decimal = verdadero o falso). ¿Se compila? Imprime una declaración en la consola para explicar por qué no y elimina la línea de código que no se compila.
- 
-
+var trueOrFalse = true
+// firstDecimal = trueOrFalse
+print("Por que son de diferente tipo")
 //:  Declara una variable y asígnale un valor de cadena. Luego, intenta asignarla a `firstDecimal` (primer decimal). ¿Se compila? Imprime una declaración en la consola para explicar por qué no y elimina la línea de código que no se compila.
  
 
