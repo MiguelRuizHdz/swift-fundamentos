d9b828913ba4108f95f736e0b9b69bc13159438a
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-06 11:17:56 -0500

    Ejercicio con una app: Seguimiento de distintos tipos

--- 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/8. Ejercicio con una app - Seguimiento de distintos tipos.xcplaygroundpage/Contents.swift
+++ 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/8. Ejercicio con una app - Seguimiento de distintos tipos.xcplaygroundpage/Contents.swift
@@ -5,11 +5,13 @@
 
 Declaraste un número de constantes y variables para hacer un seguimiento de la información relacionada con la actividad física. Declara una variable más con un valor booleano denominado `hasMetStepGoal` (se alcanzó el objetivo de pasos).
  */
- 
+var hasMetStepGoal: Bool
+// let stepsGoal = 15000
+// var steps = 1000
 
 //:  Cuando declaraste una constante para el número que representa el objetivo de pasos y una variable para el número representado por la cantidad de pasos dados hasta el momento, es probable que le hayas asignado a cada una un valor en miles. Es un número difícil de leer. Vuelve a declarar la constante y la variable, y, cuando le asignes a cada una un valor en miles, escribe el número en un formato más legible.
- 
-
+let stepsGoal = 15_000
+var steps = 1_000
 /*:
 [Anterior](@previous) | Página 8 de 10 | [Siguiente: Ejercicio: Inferencia de tipo y valores requeridos](@next)
  */
