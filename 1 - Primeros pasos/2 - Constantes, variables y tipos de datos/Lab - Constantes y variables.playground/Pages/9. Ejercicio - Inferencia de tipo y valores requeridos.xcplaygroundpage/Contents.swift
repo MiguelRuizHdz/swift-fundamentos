5159d80f85701378936696ea270a827a86131705
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-06 11:25:05 -0500

    Ejercicio: Inferencia de tipo y valores requeridos

--- 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/9. Ejercicio - Inferencia de tipo y valores requeridos.xcplaygroundpage/Contents.swift
+++ 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/9. Ejercicio - Inferencia de tipo y valores requeridos.xcplaygroundpage/Contents.swift
@@ -3,16 +3,17 @@
  
  Declara una variable denominada `name` (nombre) de tipo `String` (cadena), pero no le asignes un valor. Imprime `name` en la consola. ¿Se compila el código? Elimina el código que no se compila.
  */
- 
+var name: String
+//print(name)
 
 //:  Ahora, asígnale un valor a `name` (nombre) e imprímelo en la consola.
- 
-
+name = "Miguel"
+print(name)
 //:  Declara un valor denominado `distanceTraveled` (distancia recorrida) y configúralo en 0. No le asignes un tipo explícito.
- 
+var distanceTraveled: Double = 0
 
 //:  Ahora, asígnale un valor de 54.3 a `distanceTraveled` (distancia recorrida). ¿Se compila el código? Regresa y configura un tipo explícito en `distanceTraveled` para que compile el código.
- 
+distanceTraveled = 53.4
 
 /*:
 [Anterior](@previous) | Página 9 de 10 | [Siguiente: Ejercicio con una app: Porcentaje completado](@next)
