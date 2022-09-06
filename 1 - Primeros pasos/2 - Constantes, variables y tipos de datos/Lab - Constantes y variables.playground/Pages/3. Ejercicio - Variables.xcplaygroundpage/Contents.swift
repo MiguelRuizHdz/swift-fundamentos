512fde2ed0a1dc769a80e736ca03235ac7d458d6
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-06 10:38:09 -0500

    Ejercicio: Variables

--- 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/3. Ejercicio - Variables.xcplaygroundpage/Contents.swift
+++ 1 - Primeros pasos/2 - Constantes, variables y tipos de datos/Lab - Constantes y variables.playground/Pages/3. Ejercicio - Variables.xcplaygroundpage/Contents.swift
@@ -3,14 +3,16 @@
  
  Declara una variable `schooling` (educación) y configúrala en función del número de años escolares que has completado. Imprime `schooling` en la consola.
  */
- 
-
+var schooling: Int
+schooling = 18
+print(schooling)
 //:  Ahora, imagina que completaste un año más. Actualiza la variable `schooling` según corresponda. Imprime `schooling` en la consola.
- 
+schooling += 1
+print(schooling)
 
 //:  ¿Se compila el código que ves arriba? ¿En qué se diferencia esto de tratar de actualizar una constante? Imprime tu explicación en la consola con la función `print` (imprimir).
  
-
+print("Por que una variable si puede cambiar de valor")
 /*:
 [Anterior](@previous) | Página 3 de 10 | [Siguiente: Ejercicio con una app: Conteo de pasos](@next)
  */
